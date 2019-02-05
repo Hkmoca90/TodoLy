f133f3c48dc87009236ff6ffeb8b437540b44f8e
Hector Moreno <hector.moreno@jibjab.com>
2019-02-05 13:54:57 -0700

    Add Realm pods

---
+++ Podfile
@@ -0,0 +1,8 @@
+ platform :ios, '9.0'
+
+target 'TodoLy' do
+  use_frameworks!
+
+  # Pods for TodoLy
+    pod 'RealmSwift'
+end
---
+++ Pods/Target Support Files/Pods-TodoLy/Pods-TodoLy-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TodoLy : NSObject
+@end
+@implementation PodsDummy_Pods_TodoLy
+@end
